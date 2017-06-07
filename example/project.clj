fbfd4b8473ec8a9efbd39f507f8ae1a4499e0166
Anders Engström <anders.engstrom@leogears.com>
2017-06-08 00:06:00 +0200

    wip

--- example/project.clj
+++ example/project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [quil "2.6.0"]
+                 [org.clojure/core.async "0.3.443"]]
   :main ^:skip-aot example.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- example/src/example/core.clj
+++ example/src/example/core.clj
@@ -1,8 +1,50 @@
-(ns example.core)
+(ns example.core
+  (:require [clojure.core.async :as async]
+            [quil.core :as q]
+            [quil.middlewares.fun-mode :as funmode]))
 
-(+ 2 2)
+(defonce state-mailbox (async/chan 3))
 
-(def adder (fn [a b] (+ a b)))
+(defn setup "Create the initial state" []
+  {:background [200 200 200]
+   :color [255 128 100]
+   :x     150
+   :y     150
+   :size  10})
 
-(adder 1 2)
-;; => 3
+(defn calc-next-state
+  [{:keys [x y size] :as state}]
+
+  ;; (assoc state :x (inc x))
+
+
+
+
+(defn render
+  "Render the state 's'."
+  [{:keys [background x y size color]}]
+
+  (apply q/background background)
+  (q/ellipse-mode :center)
+  (apply q/fill color)
+  (q/ellipse x y size size))
+
+(defn next-state
+  "Poll the mailbox or use the state from current canvas."
+  [s]
+  (calc-next-state (or (async/poll! state-mailbox) s)))
+
+(defn restart!
+  ([] (restart! (setup)))
+  ([s] (async/put! state-mailbox s)))
+
+(defn run-me
+  "Display a canvas - hook up functions for drawing."
+  []
+  (q/sketch
+   :features [:keep-on-top]
+   :size [300 300]
+   :middleware [funmode/fun-mode]
+   :update #'next-state
+   :draw #'render
+   :setup #'setup))
